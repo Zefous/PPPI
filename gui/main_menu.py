b26c6bfb3568ae26916244ec2cef07978a197cc1
Zefous <61678337+Zefous@users.noreply.github.com>
2025-04-07 16:22:33 +0300

    Update main_menu.py

--- gui/main_menu.py
+++ gui/main_menu.py
@@ -11,8 +11,6 @@ def handle_menu_input(choice):
         start_new_game()
     elif choice == 2:
         load_game()
-    elif choice == 3:
-        open_gallery()
     elif choice == 4:
         show_tutorial()
     elif choice == 5:
